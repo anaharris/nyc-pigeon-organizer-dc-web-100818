1334c9898b730bb2c045af14011a79f8aa2bc39a
Ana Harris <vana1990@gmail.com>
2018-10-05 22:01:10 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,13 +10,20 @@ def nyc_pigeon_organizer(data)
   end
   names = names.flatten.uniq
   names.each do |i|
-    binding.pry
-    pigeon_list[i] = {
-      :color => [],
-      :gender => [],
-      :lives => []
-    } 
+    pigeon_list[i] = {:color => [],
+    :gender => [],
+    :lives => []}
   end
+  pigeon_list.each do |name, p_data|
+    data.each do |feature, feat_hash|
+      feat_hash.each do |feat, arr|
+        if arr.include?(name)
+          pigeon_list[name][feature] << feat.to_s
+        end
+      end
+    end
+  end
+  return pigeon_list
 end
 
 
